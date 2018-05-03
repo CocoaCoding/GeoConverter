c0f57f4d9aa70fefffc2526141d5fa262e87c7e6
CocoaCoding <holgerhinzberg@holgerhgsimac10.fritz.box>
2018-05-03 23:36:49 +0200

    Initial Commit

---
+++ GeoConverter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GeoConverter
+//
+//  Created by Holger Hinzberg on 03.05.18.
+//  Copyright © 2018 Holger Hinzberg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
